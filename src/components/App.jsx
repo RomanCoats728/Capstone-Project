f21e73f785eae5a1d40bfa1dd36da62d4bf0c43f
Roman Coats <rcoats728@gmail.com>
2023-12-23 12:33:06 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,27 @@ import "../style/index.css";
 import { Link, Route, Routes } from "react-router-dom";
 import Register from "./Login/Register";
 import Login from "./Login/Login";
-import { createBrowserRouter } from "react-router-dom";
+import Products from "./Products.js/Products";
+
+
+
 
 function App() {
+ 
   return (
+   
     <div id="Main">
+     
       <div id="navbar">
         <Link to="/">Login</Link>
         <Link to="/Register">Register</Link>
+        <Link to="/Products">Products</Link>
       </div>
       <div id="Main-section">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/Register" element={<Register />} />
+          <Route path="/Products" element={<Products/>}/>
         </Routes>
       </div>
     </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+
+export default function Header(){
+    return(
+        <div className="Header" >
+        <div className="header-title">
+        <h1>The Super Store</h1>
+        </div>
+        </div>
+    );
+}
